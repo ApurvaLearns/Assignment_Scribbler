f88d497ad7070bedc7641cc2476e7bb1727ce120
Apurva Jain97 <apurja97@in.ibm.com>
2021-05-06 18:10:50 +0530

    Part B Changes

---
+++ scripts/post.js
@@ -0,0 +1,91 @@
+showContents();
+
+function showContents()
+{
+
+    var text1= sessionStorage.getItem("name");
+    console.log(text1);
+    var text2= sessionStorage.getItem("heading");
+    console.log(text2);
+    var text3= sessionStorage.getItem("matter");
+    document.getElementById("author").innerHTML=text1;
+    document.getElementById("heading").innerHTML=text2;
+    document.getElementById("matter").innerHTML=text3;
+}
+
+function editContent()
+{
+
+    var text1=document.getElementById("editbutton").innerHTML;
+    console.log(text1);
+    if(text1=='Edit <i class="fa fa-pencil-square-o" aria-hidden="true"></i>')
+   {
+
+    console.log("Inside edit  Content");
+    var text2='<i class="fa fa-floppy-o" aria-hidden="true"></i>';
+    document.getElementById("editbutton").innerHTML="Save "+text2;
+    var text2=document.getElementById("editbutton").innerHTML;
+    console.log(text2);
+    document.getElementById("heading").contentEditable=true;
+    document.getElementById("matter").contentEditable=true;
+    document.getElementById("heading").style.border="thin solid pink";
+    document.getElementById("matter").style.border="thin solid pink";
+   }
+   else
+   {
+    console.log("Inside else  Content");
+    var text2='<i class="fa fa-pencil-square-o" aria-hidden="true"></i>';
+    document.getElementById("editbutton").innerHTML="Edit "+text2;
+    var text2=document.getElementById("editbutton").innerHTML;
+    console.log(text2);
+    document.getElementById("heading").contentEditable = false;
+    document.getElementById("matter").contentEditable=false;
+    document.getElementById("heading").style.border="0px solid transparent";
+    document.getElementById("matter").style.border="0px solid transparent";
+   }
+
+}
+
+function likeContent()
+{
+    var value=document.getElementById("likebutton").innerHTML;
+    console.log(value);
+
+    if (value =='<i class="fa fa-thumbs-up" aria-hidden="true"></i> Like')
+    {
+  document.getElementById("likebutton").innerHTML='<i class="fa fa-thumbs-up" aria-hidden="true"></i> Liked';
+        var text= document.getElementById("liketext").innerHTML;
+
+        if (text == 'Be the first one to like this!')
+        {
+            document.getElementById("liketext").innerHTML='1 person likes this!';
+        }
+
+        
+    }
+    else
+    {
+        var text= document.getElementById("liketext").innerHTML;
+        var data= text.split(" ")
+        count= data[0];
+        console.log(count);
+        count++;
+        document.getElementById("liketext").innerHTML=count+' people like this!'
+
+    }
+
+}
+
+function addComment()
+{
+    var comment= document.getElementById("comment").value;
+    console.log(comment);
+    var template='<div class="check">' + '<p class="comment-style">'+ comment + '</p>' +'</div>';
+    console.log(template);
+    document.getElementById("allcomments").innerHTML+=template;
+    document.getElementById("comment").value="";
+    document.getElementById("comment").setAttribute("placeholder","Leave a comment...");
+    
+   
+
+}
---
+++ scripts/postslist.js
@@ -0,0 +1,89 @@
+function deleteBoard1()
+{
+
+    console.log("Inside This");
+    var obj=document.getElementById("board1");
+    obj.remove();
+}
+
+function deleteBoard2()
+{
+
+    console.log("Inside This");
+    var obj=document.getElementById("board2");
+    obj.remove();
+}
+
+function deleteBoard3()
+{
+
+    console.log("Inside This");
+    var obj=document.getElementById("board3");
+    obj.remove();
+}
+
+function deleteBoard4()
+{
+
+    console.log("Inside This");
+    var obj=document.getElementById("board4");
+    obj.remove();
+}
+
+function deleteBoard5()
+{
+    console.log("Inside This");
+    var obj=document.getElementById("board5");
+    obj.remove();
+}
+
+
+function createPost1()
+{
+
+    sessionStorage.setItem("name","Srishti Gupta");
+    sessionStorage.setItem("heading","‘let’ me be a ‘const’(ant), not a ‘var’(iable)!");
+    sessionStorage.setItem("matter","Since JavaScript does not have any type-checking, either of these keywords can be used to declare a variable of any type (datatype) in JavaScript. Though all the three keywords are used for the same purpose, they are different.Web applications were written in a client/server model where the client would demand resources from the server and the server would respond with the resources. The server only responded when the client requested and would close the connection after each response.This pattern is efficient because every request to the server takes time and resources (memory, CPU, etc). To attend the next request the server must complete the previous one.So, the server attends one request at time? Well not exactly, when the server gets a new request, the request will be processed by a thread.A thread in simple words is time and resources the CPU gives to execute a small unit of instructions. With that said, the server attends multiple  requests at once, one per thread (also called thread-per-request model).");
+    window.open("post.html");
+
+}
+
+function createPost2()
+{
+
+    sessionStorage.setItem("name","Colby Fayock");
+    sessionStorage.setItem("heading","What is linting and how can it save you time?");
+    sessionStorage.setItem("matter","One of the biggest challenges in software development is time. It’s something we can’t easily get more of, but linting can help us make the most out of the time we have..Web applications were written in a client/server model where the client would demand resources from the server and the server would respond with the resources. The server only responded when the client requested and would close the connection after each response.This pattern is efficient because every request to the server takes time and resources (memory, CPU, etc). To attend the next request the server must complete the previous one.So, the server attends one request at time? Well not exactly, when the server gets a new request, the request will be processed by a thread.A thread in simple words is time and resources the CPU gives to execute a small unit of instructions. With that said, the server attends multiple requests at once, one per thread (also called thread-per-request model).");
+    window.open("post.html");
+
+}
+
+function createPost3()
+{
+
+    sessionStorage.setItem("name","Yazeed Bzadough");
+    sessionStorage.setItem("heading","How to Get More Views on Your Tech Blog");
+    sessionStorage.setItem("matter","If you're a developer with a Twitter account, you've already seen everyone and their cat start a blog, YouTube channel, or Patreon. We all want to become stars, or at the very least, a recognizable name in the industry.Web applications were written in a client/server model where the client would demand resources from the server and the server would respond with the resources. The server only responded when the client requested and would close the connection after each response.This pattern is efficient because every request to the server takes time and resources (memory, CPU, etc). To attend the next request the server must complete the previous one.So, the server attends one request at time? Well not exactly, when the server gets a new request, the request will be processed by a thread.A thread in simple words is time and resources the CPU gives to execute a small unit of instructions. With that said, the server attends multiple requests at once, one per thread (also called thread-per-request model).");
+    window.open("post.html");
+
+}
+
+function createPost4()
+{
+
+    sessionStorage.setItem("name","Cedd Burge");
+    sessionStorage.setItem("heading","How to write easily describable code");
+    sessionStorage.setItem("matter","When code is not describable using words, most people have to do some mental mapping to turn it in to words. This wastes mental energy, and you run the risk of getting the mapping wrong..Web applications were written in a client/server model where the client would demand resources from the server and the server would respond with the resources. The server only responded when the client requested and would close the connection after each response.This pattern is efficient because every request to the server takes time and resources (memory, CPU, etc). To attend the next request the server must complete the previous one.So, the server attends one request at time? Well not exactly, when the server gets a new request, the request will be processed by a thread.A thread in simple words is time and resources the CPU gives to execute a small unit of instructions. With that said, the server attends multiple requests at once, one per thread (also called thread-per-request model).");
+    window.open("post.html");
+
+}
+
+function createPost5()
+{
+
+    sessionStorage.setItem("name","Srishti Gupta");
+    sessionStorage.setItem("heading","Everything you should know about ‘module’ & ‘require’ in Node.js");
+    sessionStorage.setItem("matter","Node.js treats each JavaScript file as a separate module. For instance, if you have a file containing some code and this file is named xyz.js, then this file is treated as a module in Node, and you can say that you’ve created a module named xyz.When code is not describable using words, most people have to do some mental mapping to turn it in to words. This wastes mental energy, and you run the risk of getting the mapping wrong..Web applications were written in a client/server model where the client would demand resources from the server and the server would respond with the resources. The server only responded when the client requested and would close the connection after each response.This pattern is efficient because every request to the server takes time and resources (memory, CPU, etc). To attend the next request the server must complete the previous one.So, the server attends one request at time? Well not exactly, when the server gets a new request, the request will be processed by a thread.A thread in simple words is time and resources the CPU gives to execute a small unit of instructions. With that said, the server attends multiple requests at once, one per thread (also called thread-per-request model).");
+    window.open("post.html");
+
+}
